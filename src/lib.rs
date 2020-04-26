525c3c5d5d20532ddf66c89d2597529181cba76f
Duncan <bonsairobo@gmail.com>
2020-04-26 14:10:28 -0700

    export QuadVertices

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,5 @@ mod test_util;
 pub use face::Face;
 pub use quad::{
     boundary_quads, boundary_quads_chunked, build_quad_adjacency_lists,
-    Quad, QuadCornerInfo, QuadIndex
+    Quad, QuadCornerInfo, QuadIndex, QuadVertices,
 };
