c15e81dff65909353e83069784272a3182ee799c
Duncan Fairbanks <bonsairobo@gmail.com>
2020-08-01 12:05:58 -0700

    add license

--- src/surface_nets.rs
+++ src/surface_nets.rs
@@ -1,4 +1,27 @@
-// Adapted from the surface-nets crate by khyperia.
+// This module started as a copy from the "surface-nets" crate by khyperia. Since then, many changes
+// and optimizations have been made, but the core ideas remains. Thanks, khyperia!
+//
+// MIT License
+//
+// Copyright (c) 2019 khyperia
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 use ilattice3 as lat;
 use ilattice3::{prelude::*, Extent, HasIndexer, Indexer, CUBE_CORNERS};
